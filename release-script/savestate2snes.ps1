1c5d95c67628f2a27066c5a734898c4f8d70e24b
Sylvain Colinet <scolinet@gmail.com>
2019-03-16 16:10:39 +0100

    Fix savestate2snes download link

--- release-script/savestate2snes.ps1
+++ release-script/savestate2snes.ps1
@@ -1,8 +1,8 @@
 . ./../release-script/common.ps1
 
 $github_base_url = "https://github.com/Skarsnik/Savestate2Snes/releases/download/"
-$release = "v0.5.5"
-$url = "$github_base_url/v$release/Savestate2snes-$release.7z"
+$release = "0.5.5"
+$url = "$github_base_url/v$release/Savestate2snes-v$release.7z"
 
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 Invoke-WebRequest -Uri $url -OutFile "Savestate2Snes.7z"
